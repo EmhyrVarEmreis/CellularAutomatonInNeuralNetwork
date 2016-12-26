b83d7bbeddc21ec59350068815eeb3fd61c13814
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2016-12-26 15:51:17 +0100

    MainCombined - Switch to best weights

--- MainCombined.py
+++ MainCombined.py
@@ -3,6 +3,7 @@ import random
 from numpy import array
 
 from automaton import RuleParser
+from automaton.CellState import CellState
 from automaton.ProcessingFunction import get_neural_processing_function_bundle
 from automaton.SimpleProcessor import SimpleProcessor
 from automaton.World import World
@@ -51,13 +52,13 @@ if __name__ == "__main__":
     file_output_loc = 'tmp/n1'
     learn_cycles_count = 60000
     learn_reduce = True
-    world_size = [50, 50]
-    world_percentage = 50
+    world_size = [25, 25]
+    world_percentage = 55
     world_location = 'tmp/w1.txt'
     world_location_old = 'tmp/w0.txt'
     cycles_count_learning = 250
-    cycles_count_normal = 25
-    cycles_count_neural = 25
+    cycles_count_normal = 150
+    cycles_count_neural = 150
     gif_location_normal = 'tmp/w1a.gif'
     gif_location_neural = 'tmp/w1b.gif'
     processing_function_rule_location = 'resource/rule/2DA/life'
@@ -71,27 +72,35 @@ if __name__ == "__main__":
 
     # Init world
     world.make_random(world_percentage)
+    world.set_in_world(3, 3, CellState.Alive)
+    world.set_in_world(3, 4, CellState.Alive)
+    world.set_in_world(3, 5, CellState.Alive)
     world.save(world_location_old)
     world.save_as_image('tmp/0.png', 5)
 
     # Load processing function
     processor = SimpleProcessor(world, processing_function)
 
-    # Clear learning output
-    processor.clear_learning_output()
+    # # Clear learning output
+    # processor.clear_learning_output()
 
-    # Enable learning output
-    processor.enable_learning_output(True, file_learn_loc)
+    # # Enable learning output
+    # processor.enable_learning_output(True, file_learn_loc)
 
-    # Run learning cycles
-    processor.make_cycles(cycles_count_learning)
+    # # Run learning cycles
+    # processor.make_cycles(cycles_count_learning)
 
-    # Learn network
-    neural_network_combined = learn_from_file(file_learn_loc, file_output_loc, learn_cycles_count, learn_reduce,
-                                              neural_multi, neural_multi_layer1_count)
-
-    # Print success percentage
-    print("Status: " + str(neural_network_combined[1]) + '%')
+    # Load network
+    nn = SimpleLayeredNeuralNetwork()
+    nn.read_synaptic_weights('resource/learned/0.0159669230214.txt')
+    print(nn.verify(TrainingLoader.load('tmp/life_all')))
+    neural_network_combined = [nn]
+    # # Learn network
+    # neural_network_combined = learn_from_file(file_learn_loc, file_output_loc, learn_cycles_count, learn_reduce,
+    #                                           neural_multi, neural_multi_layer1_count)
+    #
+    # # Print success percentage
+    # print("Status: " + str(neural_network_combined[1]) + '%')
 
     # Disable learning output
     processor.enable_learning_output(False)
@@ -105,16 +114,16 @@ if __name__ == "__main__":
     world.load(world_location_old)
 
     # Make normal cycles GIF
-    # processor.make_cycles_gif(cycles_count_normal, gif_location_normal, 5)
-    world.save_as_image('tmp/a1.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/a2.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/a3.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/a4.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/a5.png', 5)
+    processor.make_cycles_gif(cycles_count_normal, gif_location_normal, 5)
+    # world.save_as_image('tmp/a1.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/a2.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/a3.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/a4.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/a5.png', 5)
 
     processing_function_neural = get_neural_processing_function_bundle(
         processor.processing_function_bundle[1], neural_network_combined[0]
@@ -126,13 +135,13 @@ if __name__ == "__main__":
     world.save('tmp/tmp2.txt')
 
     # Make neural processed cycles GIF
-    # processor.make_cycles_gif(cycles_count_normal, gif_location_neural, 5)
-    world.save_as_image('tmp/b1.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/b2.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/b3.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/b4.png', 5)
-    processor.make_cycle()
-    world.save_as_image('tmp/b5.png', 5)
+    processor.make_cycles_gif(cycles_count_neural, gif_location_neural, 5)
+    # world.save_as_image('tmp/b1.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/b2.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/b3.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/b4.png', 5)
+    # processor.make_cycle()
+    # world.save_as_image('tmp/b5.png', 5)
