ba9bb6daeadcf96713d262d7107e42f7016b5110
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-01-06 23:14:51 +0100

    MainSearch - Support for multi-layered networks and mutations when no progress

--- MainSearch.py
+++ MainSearch.py
@@ -5,17 +5,17 @@ import numpy as np
 
 from neural import TrainingLoader
 from neural.SimpleLayeredNeuralNetwork import SimpleLayeredNeuralNetwork
-from neural.SimpleNeuralNetwork import SimpleNeuralNetwork
 
 
 def main(argv):
     opt_tries = 1000
     opt_cycles = 30001
-    opt_multi = True
-    opt_multi_neural_num = 10
+    opt_multi_neural = '9-2'
     opt_every = True
     opt_step = 1000
     opt_max_error = 0.2734375
+    opt_mutations = 5
+    opt_mutation_value = .05
     opt_folder = 'tmp/search/'
     opt_input_file = 'resource/life_all'
     opt_weights_file = None
@@ -25,13 +25,14 @@ def main(argv):
     if len(argv) > 0:
         opt_tries = int(argv[0])
         opt_cycles = int(argv[1])
-        opt_multi = str(argv[2]).lower() == 't'
-        opt_multi_neural_num = int(argv[3])
-        opt_every = str(argv[4]).lower() == 't'
-        opt_step = int(argv[5])
-        opt_max_error = float(argv[6])
-        if len(argv) > 7:
-            opt_weights_file = argv[7]
+        opt_multi_neural = int(argv[2])
+        opt_every = str(argv[3]).lower() == 't'
+        opt_step = int(argv[4])
+        opt_max_error = float(argv[5])
+        opt_mutations = int(argv[6]) if argv[5].isdigit() else 0
+        opt_mutation_value = float(argv[7])
+        if len(argv) > 8:
+            opt_weights_file = argv[8]
 
     training_set_tmp = TrainingLoader.load(opt_input_file)
 
@@ -42,18 +43,21 @@ def main(argv):
 
     best_of_bests = 1
 
-    if opt_multi:
-        network = SimpleLayeredNeuralNetwork()
-        network.add_layer(opt_multi_neural_num, len(training_set_tmp[0][0]))
-        network.add_layer(1, opt_multi_neural_num)
+    network = SimpleLayeredNeuralNetwork()
+
+    opt_multi_neural = opt_multi_neural.split('-')
+
+    if len(opt_multi_neural) == 1 and int(opt_multi_neural[0]) == 1:
+        network.add_layer(1, len(training_set_tmp[0][0]))
     else:
-        network = SimpleNeuralNetwork(len(training_set_tmp[0][0]))
+        network.add_layer(int(opt_multi_neural[0]), len(training_set_tmp[0][0]))
+        for idx, val in enumerate(opt_multi_neural[1:]):
+            network.add_layer(int(val), int(opt_multi_neural[idx - 1]))
+        network.add_layer(1, int(opt_multi_neural[-1]))
 
     opt_folder = opt_folder + '/' + '.'.join(
         [str(layer.number_of_neurons) + '-' + str(layer.number_of_inputs_per_neuron) for layer in network.layers]
     )
-
-    last_network = None
 
     for i in range(opt_tries):
         print("Iteration: " + str(i + 1))
@@ -72,15 +76,23 @@ def main(argv):
             network.train_once(inputs, outputs)
             if (j % opt_step) == 0:
                 if last_error == network.error:
-                    # TODO Make mutation instead of breaking
-                    print('\tBreak - no progress')
-                    break
+                    if opt_mutations == 0:
+                        print('\tBreak - no progress')
+                        break
+                    else:
+                        print('\tMutating %d weights' % opt_mutations)
+                        for x in range(opt_mutations):
+                            layer = np.random.choice(network.layers)
+                            num = np.random.randint(0, len(layer.weights))
+                            if np.random.choice([True, False]):
+                                layer.weights[num] *= (1.0 + opt_mutation_value)
+                            else:
+                                layer.weights[num] *= (1.0 - opt_mutation_value)
+                        break
                 if network.error < opt_max_error:
                     if network.error < last_error:
                         best_weights = network.get_weights()
                 last_error = network.error
-
-        last_network = network
 
         if opt_every and best_weights:
             print("\tError: " + str(network.error))
