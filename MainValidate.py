1d6f6f4393fae690001d14f1dda80ed17ae8e74b
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-02-02 22:01:57 +0100

    Remove unnecessary TODO

--- MainValidate.py
+++ MainValidate.py
@@ -5,8 +5,8 @@ from util.training import load_training_set
 
 
 def main(argv):
-    opt_neural_network_file = 'resource/learned/2x2/15-1/0.222792568305.txt'
-    opt_training_set_location = 'resource/training_set/2x2_all'
+    opt_neural_network_file = r'F:\dev\private\github\CellularAutomatonInNeuralNetwork\resource\learned\life\9-2-1\0.0345724070658.txt'
+    opt_training_set_location = r'F:\dev\private\github\CellularAutomatonInNeuralNetwork\resource\training_set\life_all'
     if len(argv) == 2:
         opt_neural_network_file = argv[0]
         opt_training_set_location = argv[1]
